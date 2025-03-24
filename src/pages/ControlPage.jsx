8ba9fe2f1f5fc3f3f42d3c21b73d2099b30409df
Khai-vuong <khai.vuongquang@hcmut.edu.vn>
2025-03-24 11:17:52 +0700

    dnd added

--- src/pages/ControlPage.jsx
+++ src/pages/ControlPage.jsx
@@ -1,26 +1,33 @@
 import NavBar from "../components/Navbar.jsx";
 import Drag_n_drop from "../components/Drag_n_drop.jsx";
 
+import { useState } from "react";
+import useStore from "../utils/useStore.js";
+
 function ControlPage() {
-  // const maxFloor = 2;
+  // Ensure useStore is correctly used
+  const shapes = useStore();
+  const maxFloor = 2;
 
-  // const [rectangle_count, setRectangle_count] = useState(0);
-  // const [floor_count, setFloor_count] = useState(0);
-  // const [device_count, setDevice_count] = useState(0);
-  // const [sensor_count, setSensor_count] = useState(0);
+  const [rectangle_count, setRectangle_count] = useState(0);
+  const [floor_count, setFloor_count] = useState(0);
+  const [device_count, setDevice_count] = useState(0);
+  const [sensor_count, setSensor_count] = useState(0);
 
-  // function addRectangle() {
-  //   setRectangle_count(rectangle_count + 1);
-  // addElement({
-  //   //Add to Zustand
-  //   id: `rectangle-${rectangle_count}`,
-  //   type: "rectangle",
-  //   x: 0,
-  //   y: 0,
-  //   width: 100,
-  //   height: 100,
-  // });
-  // }
+  function addRectangle() {
+    alert("Rectangle added");
+
+    setRectangle_count(rectangle_count + 1);
+    shapes.addElement({
+      // Add to Zustand
+      id: `rectangle-${rectangle_count}`,
+      type: "rectangle",
+      x: 0,
+      y: 0,
+      width: 100,
+      height: 100,
+    });
+  }
 
   return (
     <div className="flex flex-row gap-4 w-screen h-screen bg-blue-300">
@@ -38,7 +45,9 @@ function ControlPage() {
           <div className="buttons flex flex-col gap-2 w-1/4 mt-4 ml-4">
             <button className="btn btn-warning">Add sensor</button>
             <button className="btn btn-warning">Add devide</button>
-            <button className="btn btn-warning">Add rectangle</button>
+            <button onClick={addRectangle} className="btn btn-warning">
+              Add rectangle
+            </button>
             <button className="btn btn-warning">Add floor</button>
           </div>
         </div>
